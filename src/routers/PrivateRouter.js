7efa803a7b390a477ebc39eddf3d27c463b1ed64
Terror009 <johnivanduiza09@gmail.com>
2022-05-16 21:06:12 +0800

    updating data

--- src/routers/PrivateRouter.js
+++ src/routers/PrivateRouter.js
@@ -8,35 +8,35 @@ function PrivateRouter({ Component, ...rest }) {
   });
   useEffect(() => {
     let key = "";
-
+    let session_key = {};
     const fetchData = async () => {
       if (key === "") {
         return false;
       } else {
-        key = JSON.parse(localStorage.getItem("user"));
-        const session_key = {
-          session_key: key.session_key,
+        key = JSON.parse(localStorage.getItem("user"))
+        session_key = {
+          session_key: key.session_key
         };
-
-        await axios({
-          method: "POST",
-          url: API.user.findUser,
-          data: JSON.stringify(session_key),
-        })
-          .then((response) => {
-            console.log(response.data);
-            response.data.map((index) => {
-              const obj = {
-                user_id: index.user_id,
-                session_key: index.session_key,
-              };
-              localStorage.setItem("user", JSON.stringify(obj));
-            });
-          })
-          .catch(({ response }) => {
-            console.log(response);
-          });
       }
+      await axios({
+        method: "POST",
+        url: API.user.findUser,
+        data: JSON.stringify(session_key),
+      })
+        .then((response) => {
+          console.log(response.data);
+          response.data.map((index) => {
+            const obj = {
+              user_id: index.user_id,
+              session_key: index.session_key,
+            };
+            localStorage.setItem("user", JSON.stringify(obj));
+          });
+        })
+        .catch(({ response }) => {
+          console.log(response);
+        });
+          
     };
     fetchData();
   }, []);
